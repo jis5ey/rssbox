2284acc1b8198202d08cdfc0b025ef21b9a82371
jis5ey <bvhml9py6lel@opayq.com>
2019-04-25 21:43:02 +0200

    add user agent
    trying a workaround for: "Too Many Requests", "error": 429

--- app/reddit.rb
+++ app/reddit.rb
@@ -5,10 +5,13 @@ class RedditError < HTTPError; end
 
 class Reddit < HTTP
   BASE_URL = "https://www.reddit.com/r"
+  HEADERS = {
+    "User-Agent" => "web:com.herokuapp.rssboxmyfork:v0.1 (by /u/ano-nimuss)"
+  }
   ERROR_CLASS = RedditError
 end
 
 error RedditError do |e|
   status 503
-  "There was a problem talking to Mixcloud. Please try again in a moment."
+  "There was a problem talking to Reddit. Please try again in a moment."
 end
